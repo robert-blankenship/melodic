7273463a2c4ab7cdbae8e792d1b6208dd40157d0
Robert Blankenship <rablanken13@gmail.com>
2016-03-15 21:10:49 -0700

    Removes Wavebender examples.

--- player/lib/examples/square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from wavebender import *
-from math import cos
-
-channels = ((square_wave(440.0, amplitude=0.1),),)
-
-samples = compute_samples(channels, 44100 * 60 * 1)
-write_wavefile(stdout, samples, 44100 * 60 * 1, nchannels=1)
